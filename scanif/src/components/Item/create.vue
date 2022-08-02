8f4a97d703c04a3fad274f38a3a3882eb29f9cc2
thiago5171 <tgazaroli@gmail.com>
2022-08-02 11:59:44 -0300

    add input fields

--- scanif/src/components/Item/create.vue
+++ scanif/src/components/Item/create.vue
@@ -7,11 +7,86 @@
           <q-tooltip class="bg-white text-black">Fechar</q-tooltip>
         </q-btn>
       </q-bar>
+
+      <div class="q-pa-sm">
+        <q-card class="q-m-sm">
+          <q-card-section class="q-mt-sm center">
+            <div class="text-h6 absolute-center text-black">
+              Registro de item
+            </div>
+          </q-card-section>
+          <div class="q-pa-md">
+            <div
+              class="q-gutter-md"
+              style="display: flex; flex-direction: column"
+            >
+              <q-input
+                outlined
+                v-model="text"
+                label="Tombamento"
+                :dense="dense"
+              />
+              <q-input
+                outlined
+                v-model="text"
+                label="Denominação"
+                :dense="dense"
+              />
+              <q-select
+                outlined
+                v-model="text"
+                label="Localidade"
+                :dense="dense"
+              />
+              <q-input outlined v-model="text" label="Termo" :dense="dense" />
+              <q-input
+                outlined
+                v-model="text"
+                label="Valor(R$)"
+                :dense="dense"
+              />
+              <q-input
+                outlined
+                v-model="text"
+                label="Tombamento antigo"
+                :dense="dense"
+              />
+              <q-input
+                outlined
+                v-model="text"
+                label="Estado do bem"
+                :dense="dense"
+              />
+              <q-input
+                outlined
+                v-model="text"
+                label="Situação"
+                :dense="dense"
+              />
+              <q-input
+                outlined
+                v-model="text"
+                label="Número de Série"
+                :dense="dense"
+              />
+
+              <q-input
+                outlined
+                v-model="text"
+                label="Observação"
+                :dense="dense"
+              />
+            </div>
+          </div>
+          <q-separator inset />
+          <q-input />
+        </q-card>
+      </div>
     </q-card>
   </q-dialog>
 </template>
 <script>
-import { useStore } from "src/store";
+import { useStore } from "vuex";
 import { computed } from "vue";
 export default {
   props: ["modal"],
