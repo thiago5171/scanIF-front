8062ad364e4c36dead33c2add210e4ff76c2b30c
thiago5171 <tgazaroli@gmail.com>
2022-08-05 01:09:18 -0300

    add logs

--- scanif/src/components/Item/QrReader.vue
+++ scanif/src/components/Item/QrReader.vue
@@ -46,7 +46,68 @@ export default {
       this.$emit("closeDialog");
     },
     async onInit(promise) {
-      s;
+      try {
+        const { capabilities } = await promise;
+
+        // successfully initialized
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          this.error = "Permissão da camera negada";
+        } else if (error.name === "NotFoundError") {
+          this.$emit("closeDialog");
+          Notify.create({
+            color: "red-5",
+            textColor: "white",
+            icon: "priority_high",
+            message: "Nenhuma camera instalada neste dispositivo",
+            position: "top-right",
+            timeout: 3000,
+          });
+        } else if (error.name === "NotSupportedError") {
+          this.$emit("closeDialog");
+          Notify.create({
+            color: "red-5",
+            textColor: "white",
+            icon: "priority_high",
+            message: "Erro na aplicação",
+            position: "top-right",
+            timeout: 3000,
+          });
+        } else if (error.name === "NotReadableError") {
+          this.$emit("closeDialog");
+          Notify.create({
+            color: "red-5",
+            textColor: "white",
+            icon: "priority_high",
+            message: "Talvez sua camera já esteja em uso",
+            position: "top-right",
+            timeout: 3000,
+          });
+        } else if (error.name === "OverconstrainedError") {
+          this.$emit("closeDialog");
+          Notify.create({
+            color: "red-5",
+            textColor: "white",
+            icon: "priority_high",
+            message: "Camera frontal não encontrada",
+            position: "top-right",
+            timeout: 3000,
+          });
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.$emit("closeDialog");
+
+          Notify.create({
+            color: "red-5",
+            textColor: "white",
+            icon: "priority_high",
+            message: "Erro no navegador, tente outro navegador",
+            position: "top-right",
+            timeout: 3000,
+          });
+        }
+      } finally {
+        // hide loading indicator
+      }
     },
     onDecode(decodeString) {
       this.decodeString = decodeString;
